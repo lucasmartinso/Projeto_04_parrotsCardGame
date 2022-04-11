cceff1a6cb3d8ebbf446660472ff2504eaedf5a3
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-04-11 17:18:39 -0300

    Projeto Finalizado

--- scripts.js
+++ scripts.js
@@ -6,51 +6,76 @@ while(numCartas % 2 === 1 || numCartas<4 || numCartas>14) {
     alert("Inválido, só são permitidos números pares, de 4 a 14!")
     numCartas = prompt("Quantas cartas você deseja jogar?"); 
 }  
+ 
+const gifs= ['imagens/bobrossparrot.gif','imagens/bobrossparrot.gif','imagens/explodyparrot.gif','imagens/explodyparrot.gif','imagens/fiestaparrot.gif','imagens/fiestaparrot.gif','imagens/metalparrot.gif','imagens/metalparrot.gif','imagens/revertitparrot.gif','imagens/revertitparrot.gif','imagens/tripletsparrot.gif','imagens/tripletsparrot.gif','imagens/ÍbisSC.png','imagens/ÍbisSC.png'];
 
-const lista= document.querySelector("ul");  
-const gifs= ['imagens/bobrossparrot.gif','imagens/explodyparrot.gif','imagens/fiestaparrot.gif','imagens/metalparrot.gif','imagens/revertitparrot.gif','imagens/tripletsparrot.gif','imagens/unicornparrot.gif'];
-
-let embaralha = gifs.slice(0,numCartas); 
+let embaralha = gifs.slice(0, numCartas); 
 embaralha.sort(comparador); 
 
 function comparador() { 
     return Math.random() - 0.5; 
-} 
+}  
 
-let carteado= document.querySelector("ul"); 
-
-for(let i=0; i<numCartas; i++) { 
-    lista.innerHTML += 
-    `<ul class="carta" onclick="viraCarta(this)"> 
-        <li class="frente"> </li>  
-        <li class="verso ibagem frente-verso"> <img  src="${gifs[i]}"/> </li>
-</ul> `
+ function baralho() { 
+    let carteado= document.querySelector("ul"); 
+    for(let i=0; i<embaralha.length; i++) { 
+        carteado.innerHTML += 
+        `<ul class="carta" onclick="viraCarta(this)"> 
+            <li class="frente"> </li>  
+            <li class="verso frente-verso"> <img  src="${embaralha[i]}"/> </li>
+    </ul> `
+    }  
  } 
+
+ baralho();
 
  function viraCarta(elemento) { 
     console.log(elemento);
-    const passaro = document.querySelector(".frente");   
-    elemento.querySelector(".frente").classList.add("frenteEscolhida");
-    const figurasAnimadas = document.querySelector(".verso");  
-    elemento.querySelector(".verso").classList.add("versoEscolhido");
+    const passaro = document.querySelector(".frenteEscolhida");   
+    const figurasAnimadas = document.querySelector(".versoEscolhido");  
+    elemento.querySelector(".frente").classList.add("frenteEscolhida"); 
+    elemento.querySelector(".frente-verso").classList.add("versoEscolhido");
 
     if( figurasAnimadas !== null) { 
-        if(elemento.querySelector(".verso").innerHTML === versoEscolhido.innerHTML) { 
-            elemento.querySelector(".verso").classList.add("par");  
-            figurasAnimadas.classList.add("par");
-            elemento.querySelector(".frente").classList.add("naoPar"); 
-            passaro.classList.add("naoPar");  
+        if(elemento.querySelector(".frente-verso").innerHTML === figurasAnimadas.innerHTML) { 
+            elemento.querySelector(".frente").classList.add("parFrente");  
+            elemento.querySelector(".frente-verso").classList.add("parVerso");  
+            figurasAnimadas.classList.add("parVerso");
+            passaro.classList.add("parFrente");  
             pares++;
         }  
 
-        setTimeout( comparador(), { 
-            elemento.querySelector(".verso").classList.add("par");
-            elemento.querySelector(".frente").classList.add("naoPar");
+        setTimeout( function() {  
+            elemento.querySelector(".frente").classList.remove("frenteEscolhida");
+            elemento.querySelector(".frente-verso").classList.remove("versoEscolhido"); 
             document.querySelector(".versoEscolhido").classList.remove("versoEscolhido");
-            document.querySelector(".frenteEscolhida").classList.remove("frenteEscolhida");
+            document.querySelector(".frenteEscolhida").classList.remove("frenteEscolhida"); 
         } , 1000);
     } 
     
-    numJogadas++;
+    numJogadas++; 
 
+} 
+
+let contador=0; 
+let idInterval= setInterval(tempo, 1000);
+function tempo() {  
+    document.querySelector(".relogio").innerHTML = `${contador}s`; 
+    if(pares === (numCartas/2)) { 
+        clearInterval(idInterval,30); 
+        alert(`Você ganhou em ${numJogadas} jogadas em ${contador}s`); 
+        reiniciar();
+    } 
+    contador++;
+} 
+
+tempo(); 
+
+function reiniciar() { 
+    let reinicio = prompt("Gostaria de reiniciar a partida? É válido somente 'sim' ou 'não'"); 
+    if(reinicio === "Sim" || reinicio === "sim") { 
+        window.location.reload();
+    } else { 
+        alert("Fim de jogo");
+    }
 }
